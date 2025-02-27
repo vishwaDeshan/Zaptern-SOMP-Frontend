9ab8b2591fb80e6f49847de686675ce2bc736051
Vishwa Waweliyadda <waweliyadda.1998@gmail.com>
2025-02-27 12:26:15 +0530

    health records basics

--- libs/somp-portal/feature/src/lib/health-info/health-info-form/health-info-form.component.ts
+++ libs/somp-portal/feature/src/lib/health-info/health-info-form/health-info-form.component.ts
@@ -1,11 +1,34 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormContainerComponent } from '@zaptern-somp-frontend/components';
 
 @Component({
   selector: 'somp-health-info-form',
   standalone: true,
   templateUrl: './health-info-form.component.html',
   styleUrls: ['./health-info-form.component.scss'],
-  imports: [CommonModule],
+  imports: [CommonModule, FormContainerComponent, ReactiveFormsModule],
 })
-export class HealthInfoFormComponent {}
+export class HealthInfoFormComponent {
+  healthForm!: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.healthForm = this.fb.group({
+      medicalConditions: [''],
+      medicalConditionsDetails: [''],
+      medications: [''],
+      medicationsDetails: [''],
+      allergies: [''],
+      allergiesDetails: [''],
+      vaccinated: [''],
+      vaccinatedDetails: [''],
+      surgeries: [''],
+      surgeriesDetails: [''],
+      accommodations: [''],
+      accommodationsDetails: [''],
+    });
+  }
+}
